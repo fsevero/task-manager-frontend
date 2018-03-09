638f63b75f1cb001ae01eac7f4172934d1fbb9ff
Severo <severo.fabricio@gmail.com>
2018-03-09 19:36:54 -0300

    Adds basic router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
-
+import { HttpModule } from "@angular/http";
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { TasksComponent } from "./tasks/tasks.component";
 import { TaskDetailComponent } from "./tasks/task-detail/task-detail.component";
+
+
+const ROUTES = RouterModule.forRoot([
+  {
+    path: 'tasks',
+    component: TasksComponent
+  },
+  {
+    path: 'tasks',
+    component: TasksComponent
+  },
+]);
+
 
 @NgModule({
   declarations: [
@@ -17,7 +31,9 @@ import { TaskDetailComponent } from "./tasks/task-detail/task-detail.component";
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule,
+    ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
