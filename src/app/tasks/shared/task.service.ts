fbb673985da43fe91b98f7d06f90a39eece3b884
Severo <severo.fabricio@gmail.com>
2018-03-10 15:21:43 -0300

    Fixes task service to memory api

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -1,5 +1,8 @@
-import { Http } from "@angular/http";
+import { Http, Response } from "@angular/http";
 import { Injectable } from "@angular/core";
+
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
 
 import { Task } from "./task.model";
 
@@ -17,29 +20,26 @@ const TASKS: Array<Task> = [
 @Injectable()
 
 export class TaskService{
+  public tasksUrl = 'api/tasks';
+
   public constructor(private http: Http) {}
 
-  public getTasks(): Promise<Task[]>{
-    let promise = new Promise<Task[]>(function(resolve, reject){
-      if (TASKS.length > 0) {
-        resolve(TASKS);
-      } else {
-        let error_msg = 'Não há tarefas!';
-        reject(error_msg);
-      }
-    });
-
-    return promise;
+  public getTasks(): Observable<Task[]>{
+    return this.http.get(this.tasksUrl)
+      .map((response: Response) => response.json() as Task[])
   }
 
 
-  public getImportantTasks(): Promise<Task[]>{
-    return Promise.resolve(TASKS.slice(0, 3));
-  }
-
-
-  public getTask(id: number): Promise<Task> {
+  public getImportantTasks(): Observable<Task[]>{
     return this.getTasks()
-      .then( tasks => tasks.find( task => task.id === id ) )
+      .map( tasks => tasks.slice(0, 3) );
+  }
+
+
+  public getTask(id: number): Observable<Task> {
+    let url = `${this.tasksUrl}/${id}`;
+
+    return this.http.get(url)
+      .map((response: Response) => response.json() as Task)
   }
 }
--- src/app/tasks/task-detail/task-detail.component.ts
+++ src/app/tasks/task-detail/task-detail.component.ts
@@ -24,7 +24,7 @@ export class TaskDetailComponent implements OnInit {
   public ngOnInit() {
     this.route.params
       .switchMap( (params: Params) => this.taskService.getTask(+params['id']) )
-      .subscribe( (task: Task) => this.task = task )
+      .subscribe( (task: Task) => this.task = task );
   }
 
   public goBack() {
