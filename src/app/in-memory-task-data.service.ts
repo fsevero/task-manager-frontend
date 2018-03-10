d74c225148a89764ac18d136b2f70af6b254e8e2
Severo <severo.fabricio@gmail.com>
2018-03-10 14:53:16 -0300

    Adds more tasks in de in memory database

--- src/app/in-memory-task-data.service.ts
+++ src/app/in-memory-task-data.service.ts
@@ -14,6 +14,8 @@ export class InMemoryTaskDataService implements InMemoryDbService{
       { id: 5, title: 'Fazer tarefa 5' },
       { id: 6, title: 'Fazer tarefa 6' },
       { id: 7, title: 'Fazer tarefa 7' },
+      { id: 8, title: 'Fazer tarefa 8' },
+      { id: 9, title: 'Fazer tarefa 9' },
     ];
 
     return { tasks };
