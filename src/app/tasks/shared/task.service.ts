a056d12e0e96c399827c3c5a9c26fb0020a316a1
Severo <severo.fabricio@gmail.com>
2018-03-10 15:27:08 -0300

    Removes unused constant TASKS

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -6,16 +6,6 @@ import "rxjs/add/operator/map";
 
 import { Task } from "./task.model";
 
-const TASKS: Array<Task> = [
-  { id: 1, title: 'Fazer tarefa 1' },
-  { id: 2, title: 'Fazer tarefa 2' },
-  { id: 3, title: 'Fazer tarefa 3' },
-  { id: 4, title: 'Fazer tarefa 4' },
-  { id: 5, title: 'Fazer tarefa 5' },
-  { id: 6, title: 'Fazer tarefa 6' },
-  { id: 7, title: 'Fazer tarefa 7' },
-];
-
 
 @Injectable()
 
@@ -23,6 +13,7 @@ export class TaskService{
   public tasksUrl = 'api/tasks';
 
   public constructor(private http: Http) {}
+
 
   public getTasks(): Observable<Task[]>{
     return this.http.get(this.tasksUrl)
