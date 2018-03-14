e06ea03bbe7aa9d2f67117d774db5209ef1b4015
Severo <severo.fabricio@gmail.com>
2018-03-13 23:01:40 -0300

    Adds create task

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -38,6 +38,16 @@ export class TaskService{
       .map((response: Response) => response.json() as Task)
   }
 
+  public createTask(task: Task): Observable<Task> {
+    let url = `${this.tasksUrl}`;
+    let body = JSON.stringify(task);
+    let headers = new Headers({ 'Content-type': 'application/json' });
+
+    return this.http.post(url, body, { headers: headers })
+      .catch(this.handleErrors)
+      .map((response: Response) => response.json() as Task );
+  }
+
   public updateTask(task: Task): Observable<Task> {
     let url = `${this.tasksUrl}/${task.id}`;
     let body = JSON.stringify(task);
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -10,19 +10,35 @@ import { TaskService } from "./shared/task.service";
 
 export class TasksComponent implements OnInit {
   public tasks: Array<Task>;
-  public selectedTask: Task;
+  public newTask: Task;
 
-  public constructor(private taskService: TaskService) { }
+  public constructor(private taskService: TaskService) {
+    this.newTask = new Task(null, '');
+  }
 
   public ngOnInit() {
     this.taskService.getTasks()
       .subscribe(
         tasks => this.tasks = tasks,
-        error => alert("Ocorreu um erro no servidor, tente mais tarde!")
+        error => alert('Ocorreu um erro no servidor, tente mais tarde!')
       );
   }
 
-  public onSelect(task: Task): void {
-    this.selectedTask = task;
+  public createTask() {
+    this.newTask.title = this.newTask.title.trim();
+
+    if ( !this.newTask.title ) {
+      alert('A tarefa deve conter um título!');
+      return;
+    }
+
+    this.taskService.createTask(this.newTask)
+      .subscribe(
+        task => {
+          this.tasks.push(task);
+          this.newTask = new Task(null, '');
+        },
+        () => alert('Ocorreu um erro no servidor, tente mais tarde!')
+      );
   }
 }
