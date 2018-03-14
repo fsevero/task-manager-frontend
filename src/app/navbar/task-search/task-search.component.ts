edf8cda1a080e7a6666c39404f90377baaf4f444
Severo <severo.fabricio@gmail.com>
2018-03-14 19:52:46 -0300

    Improves search component

--- src/app/navbar/task-search/task-search.component.ts
+++ src/app/navbar/task-search/task-search.component.ts
@@ -6,6 +6,8 @@ import { TaskService } from "../../tasks/shared/task.service";
 
 import { Subject } from "rxjs/Subject";
 import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/debounceTime";
+import "rxjs/add/operator/distinctUntilChanged";
 import "rxjs/add/operator/switchMap";
 import "rxjs/add/observable/of";
 
@@ -21,9 +23,13 @@ export class TaskSearchComponent implements OnInit{
   public constructor(private taskService: TaskService, private router: Router) { }
 
   public ngOnInit() {
-    this.searchTerms.switchMap(
-      term => term ? this.taskService.searchByTitle(term) : Observable.of<Task[]>([])
-    ).subscribe( tasks => this.tasks = tasks );
+    this.searchTerms
+      .debounceTime(350)
+      .distinctUntilChanged()
+      .switchMap(
+        term => term ? this.taskService.searchByTitle(term) : Observable.of<Task[]>([])
+      )
+      .subscribe( tasks => this.tasks = tasks );
   }
 
   public search(term: string) {
