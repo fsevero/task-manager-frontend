030997be3d2bf527c1741b95d8702f3bb528ab1f
Severo <severo.fabricio@gmail.com>
2018-03-16 22:02:53 -0300

    Adds SignIn to App Module and Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 
 import { DashboardComponent } from "./dashboard/dashboard.component";
+import { SignInFormComponent } from "./sign-in-form/sign-in-form.component";
 import { SignUpFormComponent } from "./sign-up-form/sign-up-form.component";
 import { TasksComponent } from "./tasks/tasks.component";
 import { TaskDetailComponent } from "./tasks/task-detail/task-detail.component";
@@ -10,6 +11,7 @@ const ROUTES = RouterModule.forRoot([
   { path: 'dashboard', component: DashboardComponent },
   { path: 'tasks', component: TasksComponent },
   { path: 'tasks/:id', component: TaskDetailComponent },
+  { path: 'sign-in', component: SignInFormComponent },
   { path: 'sign-up', component: SignUpFormComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
 ]);
