cf1a9761c13d761ef1b8ffd43186a483854aa7fe
Severo <severo.fabricio@gmail.com>
2018-03-17 11:46:39 -0300

    Removes InMemoryWebApi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,10 +24,6 @@ import { TaskService } from "./tasks/shared/task.service";
 // modules imports
 import { AppRoutingModule } from "./app-routing.module";
 
-// in memory web api
-import { InMemoryWebApiModule } from "angular-in-memory-web-api";
-import { InMemoryTaskDataService } from "./in-memory-task-data.service";
-
 // rxjs extensions and operators
 import "./rxjs-imports"
 
@@ -51,8 +47,7 @@ import * as datetimepicker from 'eonasdan-bootstrap-datetimepicker';
     FormsModule,
     ReactiveFormsModule,
     HttpModule,
-    AppRoutingModule,
-    InMemoryWebApiModule.forRoot( InMemoryTaskDataService )
+    AppRoutingModule
   ],
   providers: [
     Angular2TokenService,
